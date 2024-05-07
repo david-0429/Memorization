f9ae191437c727f8975db398eb62aeb40ba3dbde
david-0429 <86578317+david-0429@users.noreply.github.com>
2024-05-07 15:14:17 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 import timm
 
 def get_network(args, class_num=10, pretrain=False):
-    """ return given network
+    """ 
+    return given network
     """
-
     model = timm.create_model(args.model, pretrained=pretrain, num_classes=class_num)
 
-    return model
+    return model.cuda()
