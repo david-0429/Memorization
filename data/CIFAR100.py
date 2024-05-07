11b3e099d4ffc43ae58eedef995939c7c435b69c
david-0429 <86578317+david-0429@users.noreply.github.com>
2024-05-07 14:36:24 +0900

    Update CIFAR100.py

--- data/CIFAR100.py
+++ data/CIFAR100.py
@@ -1,9 +1,12 @@
 import torch
 import torchvision
 
-from utils import transformed
-from conf import CIFAR100_TRAIN_MEAN, CIFAR100_TRAIN_STD
+from data_utils import transformed
 
+
+#CIFAR100 mean, std
+CIFAR100_TRAIN_MEAN = (0.5070751592371323, 0.48654887331495095, 0.4409178433670343)
+CIFAR100_TRAIN_STD = (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)
 
 data_path = '/content/data/CIFAR100'
 
