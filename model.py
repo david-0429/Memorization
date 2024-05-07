5d73b53c647f6505e117ff241961d9c25856e608
david-0429 <86578317+david-0429@users.noreply.github.com>
2024-05-07 14:29:15 +0900

    Create model.py

---
+++ model.py
@@ -0,0 +1,12 @@
+import timm
+
+def get_network(args, class_num=10, pretrain=False):
+    """ return given network
+    """
+
+    model = timm.create_model(args.model, pretrained=pretrain, num_classes=class_num)
+
+    if args.gpu: #use_gpu
+        model = model.cuda()
+
+    return model
